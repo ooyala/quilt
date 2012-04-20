9c97dd5cf915f833ad912f89e825307cbac01d91
Jigish Patel <jigish@ooyala.com>
2012-04-20 11:40:56 -0700

    robustification for remote file fetching

--- test/quilt_test.js
+++ test/quilt_test.js
@@ -4,6 +4,9 @@ var quilt = process.env.QUILT_COV ?
 var Quilt = quilt.Quilt;
 var expect = require("expect.js");
 var exec = require("child_process").exec;
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
 
 var noop = function(){};
 
@@ -61,16 +64,38 @@ describe('quilt', function() {
   describe('instance functions', function() {
     var myQuilt = null;
     var myNoRemoteQuilt = null;
+    var server = null;
     before(function() {
+      // fake server for remote requests
+      server = http.createServer(function(request, response) {
+        var parsed = url.parse(request.url);
+        var pathname = parsed.pathname;
+        var fileContents = null;
+        try {
+          fileContents = fs.readFileSync(__dirname + "/fake_server" + pathname);
+        } catch(err) {
+          response.writeHead(404, {'Content-Type': 'application/octet-stream'});
+          response.end('');
+          return;
+        }
+        response.writeHead(200, {'Content-Type': 'application/octet-stream'});
+        response.end(fileContents);
+      }).listen(1337);
+
+      // remote enabled quilt
       myQuilt = quilt.create({
         "local_path" : __dirname + "/fake_project",
         "remote_host" : "localhost",
-        "remote_path" : "/projects"
+        "remote_port" : 1337,
+        "remote_path" : "/"
       }, noop, noop);
+
+      // local only quilt
       myNoRemoteQuilt = quilt.create({ "local_path" : __dirname + "/fake_project" }, noop, noop);
     });
 
     after(function() {
+      server.close();
     });
 
     describe('moduleNameFromFileName', function() {
@@ -212,6 +237,26 @@ describe('quilt', function() {
         });
         expect(version.footer).to.be("f1.0.0\n");
       });
+
+      it('should gracefully handle a bad manifest entries', function() {
+        var version = Quilt.loadVersion(__dirname + "/bad_project", "1.0.0");
+        expect(version).not.to.be(null);
+        expect(version.manifest).not.to.be(null);
+        expect(version.base).to.be("c\n");
+        expect(version.modules).to.eql({
+          "0" : { "dependancies" : [ "8" ], "module" : "0\n" },
+          "1" : { "dependancies" : [ "7", "9" ], "module" : "1\n" },
+          "2" : { "dependancies" : [ "8" ], "module" : "2\n" },
+          "3" : { "dependancies" : [], "module" : "3\n" },
+          "4" : { "dependancies" : [], "module" : "4\n" },
+          "5" : { "dependancies" : [], "module" : "5\n" },
+          "6" : { "dependancies" : [], "module" : "6\n" },
+          "7" : { "dependancies" : [], "module" : "7\n" },
+          "8" : { "dependancies" : [], "module" : "8\n" },
+          "9" : { "dependancies" : [], "module" : "9\n" },
+        });
+        expect(version.footer).to.be(null);
+      });
     });
 
     describe('resolveDependancies', function() {
@@ -262,6 +307,11 @@ describe('quilt', function() {
 
       it('should gracefully handle circular dependancies', function() {
         var out = myQuilt.resolveDependancies([ "5" ], version);
+        expect(out).to.be('6\n5\n');
+      });
+
+      it('should gracefully handle non-existant modules', function() {
+        var out = myQuilt.resolveDependancies([ "5", "oogabooga" ], version);
         expect(out).to.be('6\n5\n');
       });
     });
@@ -333,6 +383,30 @@ describe('quilt', function() {
           done();
         });
       });
+
+      it("should return null if version doesn't exist locally or remote", function(done) {
+        var version = {};
+        myQuilt.getVersion('3.0.0', function(theVersion) {
+          expect(theVersion).to.be(null);
+          done();
+        });
+      });
+
+      it("should return null if remote version file is empty", function(done) {
+        var version = {};
+        myQuilt.getVersion('empty', function(theVersion) {
+          expect(theVersion).to.be(null);
+          done();
+        });
+      });
+
+      it("should return null if remote version file is not a gzipped tar", function(done) {
+        var version = {};
+        myQuilt.getVersion('bad', function(theVersion) {
+          expect(theVersion).to.be(null);
+          done();
+        });
+      });
     });
 
     describe('stitch', function() {
@@ -374,6 +448,15 @@ describe('quilt', function() {
       it('should properly stitch for remote version with selector array', function(done) {
         myQuilt.stitch(["0"], "2.0.0", function(stitched, error) {
           expect(stitched).to.be('h\nc\n8\n0\nf2.0.0\n');
+          expect(error).to.be(null);
+          exec("rm -rf "+__dirname+"/fake_project/2.0.0", function(e, stdo, stde){});
+          done();
+        });
+      });
+
+      it('should properly stitch for remote version with selector function', function(done) {
+        myQuilt.stitch(function() { return true; }, "2.0.0", function(stitched, error) {
+          expect(stitched).to.be('h\nc\n8\n0\n7\n9\n1\n2\n3\n4\n5\n6\nf2.0.0\n');
           expect(error).to.be(null);
           exec("rm -rf "+__dirname+"/fake_project/2.0.0", function(e, stdo, stde){});
           done();
